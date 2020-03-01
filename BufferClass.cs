3922ac15de6322e8732d2913cb36701a15a34cf3
Morten Juul <mort79f8@edu.campusvejle.dk>
2020-03-01 11:47:37 +0100

    Add project files.

---
+++ BufferClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteToBottom
+{
+    public class BufferClass
+    {
+        private List<string> buffer;
+
+        public BufferClass()
+        {
+            buffer = new List<string>();
+        }
+
+        public List<string> Buffer
+        {
+            get { return buffer; }
+            set{ buffer = value; }
+        }
+
+        public void AddToBuffer(string str)
+        {
+            CheckBuffer();
+            Buffer.Add(str);
+        }
+
+        public void WriteFromBuffer()
+        {
+            Console.Clear();
+            for (int i = 0; i < Buffer.Count; i++)
+            {
+                WriteOnBottomLine(Buffer[i]);
+            }
+        }
+
+        private void WriteOnBottomLine(string text)
+        {
+            Console.CursorTop = Console.WindowTop + Console.WindowHeight - 2;
+            Console.CursorLeft = Console.WindowLeft;
+            Console.WriteLine(text);
+            Console.WriteLine();
+        }
+        private void CheckBuffer()
+        {
+            int maxLenght = Console.WindowHeight / 3;
+            if (buffer.Count != 0)
+            {
+                if (buffer.Count >= maxLenght)
+                {
+                    buffer.RemoveAt(0);               
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WriteToBottom
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {         
+        
+        static void Main()
+        {
+            BufferClass b = new BufferClass();
+            b.AddToBuffer("Velkommen til buffer testing");
+
+            while (true)
+            {
+                b.WriteFromBuffer();                
+                Console.Write("Prompt: ");
+                string prompt = Console.ReadLine();
+                if (prompt == "exit")
+                {
+                    Environment.Exit(1);
+                }
+                b.AddToBuffer(prompt);
+            }
+        }
+
+
+        
+
+    }
+}
